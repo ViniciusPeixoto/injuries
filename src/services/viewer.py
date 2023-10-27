d7731d37b56e0f0e371eecf6305f91b4e97701d9
Vinicius Peixoto <vinicius.opeixoto@gmail.com>
2023-10-26 21:09:02 -0300

    Removed viewer

--- src/services/viewer.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-from pandas import DataFrame
-
-
-def plt_df(df: DataFrame):
-    df.plot(kind="line")
-    plt.xlabel("Time frame")
-    plt.legend(loc="best")
-    plt.show()
